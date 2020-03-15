147eadd630349caafb971451745a6012776f72b0
DeveloperWizard83 <laaksomik@gmail.com>
2020-03-15 22:10:26 +0100

    Updates

---
+++ precache-manifest.fff457dcb7ea81b40147924fcce6c0c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bae926812aad50c01b52453d5fdcc2c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7265710197b4347e537c",
+    "url": "/monsters/static/css/main.0bacb2c7.chunk.css"
+  },
+  {
+    "revision": "aa3c85476dd53b921f9f",
+    "url": "/monsters/static/js/2.4f36d905.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.4f36d905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7265710197b4347e537c",
+    "url": "/monsters/static/js/main.d4b2eee9.chunk.js"
+  },
+  {
+    "revision": "3e333ff2dbb9589ba51b",
+    "url": "/monsters/static/js/runtime-main.ac16f973.js"
+  }
+]);
